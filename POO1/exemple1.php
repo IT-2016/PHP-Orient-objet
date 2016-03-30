2ff9d1f4fa6b7e48b5620142aaf5fc7b0618d600
ImKrach <rachid.debu@yahoo.fr>
2016-03-30 14:50:38 +0200

    Create exemple1.php

---
+++ POO1/exemple1.php
@@ -0,0 +1,12 @@
+<?php
+  class SimpleClass
+  {
+      // déclaration d'une propriété
+      public $var = 'une valeur par défaut';
+
+      // déclaration des méthodes
+      public function displayVar() {
+          echo $this->var;
+      }
+  }
+?>
